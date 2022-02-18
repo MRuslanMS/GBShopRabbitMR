69afc49ca336a1a3f0285ee0d7d9687d96564330
MRuslanMS <palnr531@gmail.com>
2022-02-18 23:19:21 +0300

    first commit

---
+++ GBShopRabbitMR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GBShopRabbitMR
+//
+//  Created by Ruslan Magomedov on 18.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
